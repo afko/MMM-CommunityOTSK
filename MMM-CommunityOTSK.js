72a50b37779a4f777b24d6d4dc7e17725884a5a1
Sungje Kim <sungje.kim@one-line.com>
2019-03-25 21:20:48 +0900

    test030

--- MMM-CommunityOTSK.js
+++ MMM-CommunityOTSK.js
@@ -39,7 +39,7 @@ Module.register('MMM-CommunityOTSK', {
     getDom: function () {
 
         var wrapper = document.createElement("div");
-
+        wrapper.className = "center";
         var div1 = document.createElement("div");
         var div2 = document.createElement("div");
         div2.id = "DIV2";
