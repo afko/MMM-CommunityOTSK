64a7bec3c050dc3bea59f63ad439c892a4073d23
Sungje Kim <sungje.kim@one-line.com>
2019-04-12 16:42:39 +0900

    scroll test2 01

--- MMM-CommunityOTSK.js
+++ MMM-CommunityOTSK.js
@@ -90,14 +90,14 @@ Module.register('MMM-CommunityOTSK', {
             wrapper.appendChild(div3);
 
 
-            setTimeout(function () {}, 2000)
-            setInterval(function () {
-                document.getElementById("DIV3").scrollBy({
-                    top: 40, // could be negative value
-                    left: 0,
-                    behavior: 'smooth'
-                });
-            }, 2500)
+            // setTimeout(function () {}, 2000)
+            // setInterval(function () {
+            //     document.getElementById("DIV3").scrollBy({
+            //         top: 40, // could be negative value
+            //         left: 0,
+            //         behavior: 'smooth'
+            //     });
+            // }, 2500)
 
 
 
--- node_helper.js
+++ node_helper.js
@@ -7,9 +7,24 @@ module.exports = NodeHelper.create({
 	socketNotificationReceived: function(notification, payload) {
 		if(notification === "START"){
 			this.config = payload;
+			
 			this.readData();
+			setTimeout(function () {}, 2000)
+			document.getElementById("DIV3").scrollBy({
+				top: 40, // could be negative value
+				left: 0,
+				behavior: 'smooth'
+			});
     			setInterval(() => {
 					this.readData();
+					setTimeout(function () {}, 2000)
+					setInterval(function () {
+						document.getElementById("DIV3").scrollBy({
+							top: 40, // could be negative value
+							left: 0,
+							behavior: 'smooth'
+						});
+					}, 2500)
     			}, this.config.updateInterval);
 		}
 	},
